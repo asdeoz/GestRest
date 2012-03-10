9b23d4ffed723d3ed6c4dd11a7564b192b185847
Carles Garcia <asdeoz@gmail.com>
2012-03-10 01:31:55 +0100

    New project added: Utils
    New control added: Utils.DateButton

---
+++ Utils/DateButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utils
+{
+    public class DateButton : System.Windows.Forms.Button
+    {
+        private DateTime _date;
+
+        public DateTime Date
+        {
+            get { return _date; }
+            set 
+            {
+                this.Text = value.Day.ToString();
+                _date = value; 
+            }
+        }
+
+        public DateButton()
+        { }
+
+        public DateButton(DateTime newDate)
+        {
+            this.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Date = newDate;
+        }
+    }
+}
