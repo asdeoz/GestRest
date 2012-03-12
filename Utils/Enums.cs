66fc2ba340f41bd3221f87ec0a3e228e2155105c
Carles Garcia <asdeoz@gmail.com>
2012-03-12 22:49:23 +0100

    New class added: Utils.Enums
    New class added: Utils.ExtClasses
    New class added: Utils.Functions

---
+++ Utils/Enums.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utils
+{
+    public static class Enums
+    {
+        public enum Dies
+        {
+            Lunes = 1,
+            Martes = 2,
+            Miercoles = 3,
+            Jueves = 4,
+            Viernes = 5,
+            Sabado = 6,
+            Domingo = 7
+        }
+
+        public enum Mesos
+        {
+            Enero = 1,
+            Febrero = 2,
+            Marzo = 3,
+            Abril = 4,
+            Mayo = 5,
+            Junio = 6,
+            Julio = 7,
+            Agosto = 8,
+            Setiembre = 9,
+            Octubre = 10,
+            Noviembre = 11,
+            Diciembre = 12
+        }
+    }
+}
---
+++ Utils/Functions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utils
+{
+    public static class Functions
+    {
+
+        public static int DiaSemanaSP(int dia)
+        {
+            if (dia == 0) return 7;
+            else return dia;
+        }
+    }
+}
